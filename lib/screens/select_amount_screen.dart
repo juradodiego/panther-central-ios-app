515cad23c15af0840ef037e6329192cc7453f472
Diego <daj85@pitt.edu>
2022-11-16 00:49:30 -0500

    Quick Fix

--- lib/screens/select_amount_screen.dart
+++ lib/screens/select_amount_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:panther_central_ios_app/screens/add_payment_screen.dart';
 import 'package:panther_central_ios_app/viewModel/user_view_model.dart';
 
-
 class SelectAmountScreen extends StatefulWidget {
   final UserViewModel? user;
 
@@ -15,7 +14,6 @@ class SelectAmountScreen extends StatefulWidget {
 }
 
 class _SelectAmountScreenState extends State<SelectAmountScreen> {
-
   @override
   Widget build(BuildContext context) {
     const PC_YELLOW = Color.fromARGB(255, 255, 185, 29);
@@ -28,22 +26,29 @@ class _SelectAmountScreenState extends State<SelectAmountScreen> {
         appBar: PreferredSize(
             preferredSize: Size.fromHeight(100), // Set this height
             child: Container(
-                color: PC_BLUE,
-                child: Row(children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.only(left: 10, top: 30.0),
-                    child: IconButton(
-                      icon: Icon(Icons.arrow_back_ios_new),
-                      iconSize: 40,
-                      color: PC_YELLOW,
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
+              color: PC_BLUE,
+              child: IntrinsicHeight(
+                child: Stack(
+                  children: [
+                    // TODO Add Font Size, Font Color
+                    const Align(child: Text('CHOOSE PAYMENT')),
+                    Positioned(
+                      left: 10,
+                      child: IconButton(
+                        icon: const Icon(Icons.arrow_back_ios),
+                        iconSize: 40,
+                        color: PC_YELLOW,
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                      ),
                     ),
-                  ),
-                  Title(color: Colors.white, child: const Text("CHOOSE PAYMENT")),
-                ]))),
-        body: SingleChildScrollView(child: Column(children: <Widget>[
+                  ],
+                ),
+              ),
+            )),
+        body: SingleChildScrollView(
+            child: Column(children: <Widget>[
           // TODO List of Payment Methods
           // TODO Add Payment Method Link
           // TODO When Payment Method is Added the Screen Updates
@@ -56,8 +61,10 @@ class _SelectAmountScreenState extends State<SelectAmountScreen> {
                   color: PC_YELLOW, borderRadius: BorderRadius.circular(20)),
               child: TextButton(
                 onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(
-                      builder: (_) => AddPaymentScreen(user)));
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (_) => AddPaymentScreen(user)));
                 },
                 child: const Text(
                   'ADD PAYMENT METHOD',
